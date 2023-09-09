49757d750476a36a5207a1d3e8c97c275d404c45
muriithi017 <muriithidennis340@gmail.com>
2023-09-09 19:54:40 +0300

    db model and site urls api

--- sharp/sharp/urls.py
+++ sharp/sharp/urls.py
@@ -1,10 +1,17 @@
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+from django.shortcuts import redirect
+    
+
+
+admin.site.site_header = "SHARPJr."
+admin.site.site_title = "Sharp"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sharpapp.urls')),
     path('accounts/', include('accounts.urls')),
     path('payment/', include('payment.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
